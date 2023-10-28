11805b0af18545a8f85fd4a4b39a89a9e4e7ae2a
Mr@UnKnOwN <44204514+VivekKamisetty@users.noreply.github.com>
2023-10-28 15:32:12 -0400

    Update Manhattan.java

--- HW04/src/edu/umb/cs681/hw04/Manhattan.java
+++ HW04/src/edu/umb/cs681/hw04/Manhattan.java
@@ -3,19 +3,15 @@ package edu.umb.cs681.hw04;
 import java.util.List;
 import java.util.stream.IntStream;
 
-public class Manhattan implements DistanceMetric{
+import java.util.List;
+import java.util.stream.IntStream;
 
- 
-    
+public class Manhattan implements DistanceMetric {
     @Override
     public double distance(List<Double> p1, List<Double> p2) {
-        double sumOfDifferences =
-        	IntStream
-        		.range(0, p1.size())
-        		.mapToDouble(i -> p1.get(i) - p2.get(i))
-        		.map(n -> Math.abs(n))
-        		.sum();
-        	return sumOfDifferences;
+        double sumOfDifferences = IntStream.range(0, p1.size())
+                .mapToDouble(i -> Math.abs(p1.get(i) - p2.get(i)))
+                .sum();
+        return sumOfDifferences;
     }
-    
 }
