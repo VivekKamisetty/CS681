d5878ca4cf42285d8b9a3a753e8b5bd9be3dd105
Mr@UnKnOwN <44204514+VivekKamisetty@users.noreply.github.com>
2023-10-28 15:31:41 -0400

    Update DistanceTest.java

--- HW04/src/edu/umb/cs681/hw04/DistanceTest.java
+++ HW04/src/edu/umb/cs681/hw04/DistanceTest.java
@@ -2,30 +2,44 @@ package edu.umb.cs681.hw04;
 
 //import edu.umb.cs681.hw04.Distance;
 //import edu.umb.cs681.hw04.DistanceMetric;
-//import edu.umb.cs681.hw04.Euclidean;
 //import edu.umb.cs681.hw04.Manhattan;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.List;
+import java.util.Random;
+
+
 
 public class DistanceTest {
     public static void main(String[] args) {
         int numPoints = 1000;
         int numDimensions = 100;
-        List<List<Double> > points = generateRandomPoints(numPoints, numDimensions);
+        List<List<Double>> points = generateRandomPoints(numPoints, numDimensions);
 
-        // Test Euclidean distance for the first two points
-        DistanceMetric euclidean = new Euclidean();
-        double euclideanDistance = Distance.get(points.get(0), points.get(1), euclidean);
-        System.out.println("Euclidean distance between the first two points: " + euclideanDistance);
-//        System.out.println(euclideanDistance);
+        System.out.println("Generated Points:");
+        for (List<Double> point : points) {
+            System.out.println(point);
+        }
 
-        // Test Manhattan distance for the first two points
-        DistanceMetric manhattan = new Manhattan();
-        double manhattanDistance = Distance.get(points.get(0), points.get(1), manhattan);
-        System.out.println("Manhattan distance between the first two points: " + manhattanDistance);
-        System.out.println(manhattanDistance);
+        // Generating distance matrix using Euclidean metric
+        List<List<Double>> euclideanMatrix = calculateDistanceMatrix(points, new Euclidean());
+
+        // Display Euclidean distance matrix
+        System.out.println("\nEuclidean Distance Matrix:");
+        for (List<Double> row : euclideanMatrix) {
+            System.out.println(row);
+        }
+
+        List<List<Double>> manhattanMatrix = calculateDistanceMatrix(points, new Manhattan());
+
+        // Display Manhattan distance matrix
+        System.out.println("\nManhattan Distance Matrix:");
+        for (List<Double> row : manhattanMatrix) {
+            System.out.println(row);
+        }
     }
 
     private static List<List<Double>> generateRandomPoints(int numPoints, int numDimensions) {
@@ -42,5 +56,29 @@ public class DistanceTest {
         }
 
         return points;
+    }
+
+    private static List<List<Double>> calculateDistanceMatrix(List<List<Double>> points, DistanceMetric metric) {
+        int numOfPoints = points.size();
+        List<List<Double>> distanceMatrix = new ArrayList<>();
+
+        IntStream.range(0, numOfPoints).forEach(i -> {
+            List<Double> row = new ArrayList<>();
+            List<Double> current = points.get(i);
+
+            IntStream.range(0, numOfPoints).forEach(j -> {
+                List<Double> peer = points.get(j);
+                double distance = calculateDistance(metric, current, peer);
+                row.add(distance);
+            });
+
+            distanceMatrix.add(row);
+        });
+
+        return distanceMatrix;
+    }
+
+    private static double calculateDistance(DistanceMetric metric, List<Double> p1, List<Double> p2) {
+        return metric.distance(p1, p2);
     }
 }
