0550b3fdbf9d51738f7f5ff8fd432a347a0c9584
Mr@UnKnOwN <44204514+VivekKamisetty@users.noreply.github.com>
2023-12-20 04:14:01 -0500

    Update Link.java

--- HW10/src/edu/umb/cs681/hw10/Link.java
+++ HW10/src/edu/umb/cs681/hw10/Link.java
@@ -1,40 +1,123 @@
 package edu.umb.cs681.hw10;
 
 import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class Link extends FSElement {
+public class Directory extends FSElement {
+
+    private LinkedList<FSElement> children = new LinkedList<FSElement>();
+    private LinkedList<Directory> subDirectories = new LinkedList<Directory>();
+    private LinkedList <File> files = new LinkedList<File>();
+    private ReentrantLock lock = new ReentrantLock();
 
 
-    private FSElement target;
-
-    Link(Directory parent, String name, int size, LocalDateTime creationTime, FSElement target) {
+    Directory(Directory parent, String name, int size, LocalDateTime creationTime) {
         super(parent, name, size, creationTime);
         // TODO Auto-generated constructor stub
-        this.target = target;
+        if (parent != null)
+        {
+            //System.out.println("Appending....");
+            parent.appendChild(this);
+
+        }
+        //System.out.println("else Appending....");
+
     }
 
-    public void setTarget(FSElement target) {
-        this.target = target;
+
+
+    public LinkedList<FSElement> getChildren() {
+    	 lock.lock();
+         try {
+             return this.children;
+         } finally {
+             lock.unlock();
+         }
     }
 
-    public FSElement getTarget() {
-        return target;
+    public void appendChild(FSElement child) {
+        
+        lock.lock();
+        try {
+            this.children.add(child);
+            child.setParent(this);
+        } finally {
+            lock.unlock();
+        }
     }
 
+    public int countChildren() {
+        
+        lock.lock();
+        try {
+            return getChildren().size();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public LinkedList<Directory> getSubDirectories() {
+         lock.lock();
+         try {
+             for (FSElement child : this.children) {
+                 if (child.isDirectory()) {
+                     subDirectories.add((Directory) child);
+                 }
+             }
+         } finally {
+             lock.unlock();
+         }
+         return subDirectories;
+    }
+
+    public LinkedList<File> getFiles() {
+    	 lock.lock();
+         try {
+             for (FSElement child : this.children) {
+                 if (child.isFile()) {
+                     files.add((File) child);
+                 }
+             }
+         } finally {
+             lock.unlock();
+         }
+         return files;
+    }
+
+    public int getTotalSize() {
+    	int totalSize = this.getSize();
+        lock.lock();
+        try {
+            for (FSElement element : this.children) {
+                if (element.isDirectory()) {
+                    totalSize += ((Directory) element).getTotalSize();
+                } else {
+                    totalSize += element.getSize();
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
+        return totalSize;
+    }
     @Override
     public boolean isDirectory() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+
+
+    @Override
+    public boolean isFile() {
         // TODO Auto-generated method stub
         return false;
     }
 
     @Override
-    public boolean isFile() {
-        return false;
-    }
-
-    @Override
     public boolean isLink() {
-        return true;
+        return false;
     }
 
 
