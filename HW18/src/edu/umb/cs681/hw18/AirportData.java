a51d882bc1e58c36c49c16e056e6549df4f1777b
Mr@UnKnOwN <44204514+VivekKamisetty@users.noreply.github.com>
2023-12-19 20:51:31 -0500

    Add files via upload

---
+++ HW18/src/edu/umb/cs681/hw18/AirportData.java
@@ -0,0 +1,46 @@
+package edu.umb.cs681.hw18;
+
+class AirportData {
+    private int year;
+    private int month;
+    private int loganPassengers;
+    private int loganIntlFlights;
+    private float hotelOccupancyRate;
+    private float hotelAvgDailyRate;
+    private int totalJobs;
+    private float unemploymentRate;
+    private float laborForceParticipationRate;
+
+    public AirportData(int year, int month, int loganPassengers, int loganIntlFlights, float hotelOccupancyRate,
+                       float hotelAvgDailyRate, int totalJobs, float unemploymentRate, float laborForceParticipationRate) {
+        this.year = year;
+        this.month = month;
+        this.loganPassengers = loganPassengers;
+        this.loganIntlFlights = loganIntlFlights;
+        this.hotelOccupancyRate = hotelOccupancyRate;
+        this.hotelAvgDailyRate = hotelAvgDailyRate;
+        this.totalJobs = totalJobs;
+        this.unemploymentRate = unemploymentRate;
+        this.laborForceParticipationRate = laborForceParticipationRate;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getLoganPassengers() {
+        return loganPassengers;
+    }
+
+    public float getHotelOccupancyRate() {
+        return hotelOccupancyRate;
+    }
+
+    public int getTotalJobs() {
+        return totalJobs;
+    }
+
+    public float getUnemploymentRate() {
+        return unemploymentRate;
+    }
+}
