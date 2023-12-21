7dd52962288a38230b58d1c4d78b0f8b7326ef1f
Mr@UnKnOwN <44204514+VivekKamisetty@users.noreply.github.com>
2023-12-20 20:59:35 -0500

    Update FileSystemCrawler.java

--- HW16/src/edu/umb/cs681/hw16/FileSystemCrawler.java
+++ HW16/src/edu/umb/cs681/hw16/FileSystemCrawler.java
@@ -1,71 +1,52 @@
 package edu.umb.cs681.hw16;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class FileSystemCrawler {
-	private static Queue<File> sharedQueue = new ConcurrentLinkedQueue<>();
-	private static AtomicBoolean terminateFlag = new AtomicBoolean(false);
+    private static ConcurrentLinkedQueue<File> sharedQueue = new ConcurrentLinkedQueue<>();
+    private static volatile boolean terminateFlag = false;
     private static ThreadLocal<FileCrawlingVisitor> threadLocalFileCrawler = ThreadLocal.withInitial(() -> new FileCrawlingVisitor());
-	
-   
-    
-    public static void main(String [] args) {
-		TestFixatureInitializer fixatureInitializer = new TestFixatureInitializer();
-		
-		fixatureInitializer.FileSystem1();
+
+    public static void main(String[] args) {
+        TestFixatureInitializer fixatureInitializer = new TestFixatureInitializer();
+        fixatureInitializer.FileSystem1();
         fixatureInitializer.FileSystem2();
         fixatureInitializer.FileSystem3();
-		
-		Thread thread1 = new Thread(() -> {
-			FileCrawlingVisitor fileCrawler1 = threadLocalFileCrawler.get();
-			fixatureInitializer.DriveC.accept(fileCrawler1);
-            sharedQueue.addAll(fileCrawler1.getFiles());
-            terminateFlag.set(true);
-		});
-		
-		Thread thread2 = new Thread(() -> {
-			FileCrawlingVisitor fileCrawler2 = threadLocalFileCrawler.get();
-			fixatureInitializer.DriveD.accept(fileCrawler2);
-            sharedQueue.addAll(fileCrawler2.getFiles());
-            terminateFlag.set(true);
-		});
-		
-		Thread thread3 = new Thread(() -> {
-			FileCrawlingVisitor fileCrawler3 = threadLocalFileCrawler.get();
-			fixatureInitializer.DriveE.accept(fileCrawler3);
-	        sharedQueue.addAll(fileCrawler3.getFiles());
-	        terminateFlag.set(true);
-		});
-		
-		thread1.start();
-		thread2.start();
-		thread3.start();
-		
-		try {
-			while(!terminateFlag.get()) {
-				Thread.sleep(100);
-			}
-			
-			thread1.interrupt();
-			thread2.interrupt();
-			thread3.interrupt();
-			
-			List<File> allFiles;
-			 synchronized (FileSystemCrawler.class) {
-	                allFiles = new ArrayList<>(sharedQueue);
-	            }
 
-	            System.out.println("Identified Files: ");
-	            for (File file : allFiles) {
-	                System.out.println(file.getName());
-	            }
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+        Thread thread1 = startCrawlingThread(fixatureInitializer.DriveC);
+        Thread thread2 = startCrawlingThread(fixatureInitializer.DriveD);
+        Thread thread3 = startCrawlingThread(fixatureInitializer.DriveE);
 
+        try {
+            while (!terminateFlag) {
+                Thread.sleep(100);
+            }
+
+            terminateCrawlingThreads(new Thread[]{thread1, thread2, thread3});
+
+            System.out.println("Identified Files:");
+            for (File file : sharedQueue) {
+                System.out.println(file.getName());
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Thread startCrawlingThread(Directory drive) {
+        Thread thread = new Thread(() -> {
+            FileCrawlingVisitor fileCrawler = threadLocalFileCrawler.get();
+            drive.accept(fileCrawler);
+            sharedQueue.addAll(fileCrawler.getFiles());
+            terminateFlag = true;
+        });
+        thread.start();
+        return thread;
+    }
+
+    private static void terminateCrawlingThreads(Thread[] threads) {
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
+    }
 }
