966c5e28f672652c6b497397b777b9ca51fe642d
Mr@UnKnOwN <44204514+VivekKamisetty@users.noreply.github.com>
2023-12-20 21:21:39 -0500

    Update TestConcurrentObservers.java

--- HW17/src/edu/umb/cs681/hw17/TestConcurrentObservers.java
+++ HW17/src/edu/umb/cs681/hw17/TestConcurrentObservers.java
@@ -5,39 +5,46 @@ import java.util.List;
 
 public class TestConcurrentObservers {
 	
-	public static void main(String[] args) {
-		StackQuoteObservable observable = new StackQuoteObservable();
-		TableObserver tableObserver = new TableObserver();
-		LineChartObserver lineChartObserver = new LineChartObserver();
-		Observer3D observer3D = new Observer3D();
-		
-		observable.addObserver(tableObserver);
-		observable.addObserver(lineChartObserver);
-		observable.addObserver(observer3D);
-		
-		int numThreads = 10;
-		
-        List<Thread> threads = new ArrayList<>();
-        List<DataHandler> dataHandlers = new ArrayList<>();
+	 public static void main(String[] args) {
+	        StackQuoteObservable observable = new StackQuoteObservable();
+	        TableObserver tableObserver = new TableObserver();
+	        LineChartObserver lineChartObserver = new LineChartObserver();
+	        Observer3D observer3D = new Observer3D();
 
-		for (int i = 0; i < numThreads; i++) {
-			DataHandler dataHandler = new DataHandler(observable);
-			Thread thread = new Thread(dataHandler);
-			threads.add(thread);
-			dataHandlers.add(dataHandler);
-			thread.start();
-		}
-		
-		threads.forEach(Thread::start);
-		
-		try {
-			Thread.sleep(10000);
-		} catch  (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		for (DataHandler handler : dataHandlers) {
-            	handler.stop();
-        }	}
+	        observable.addObserver(tableObserver);
+	        observable.addObserver(lineChartObserver);
+	        observable.addObserver(observer3D);
+
+	        int numThreads = 10;
+
+	        List<Thread> threads = new ArrayList<>();
+	        List<DataHandler> dataHandlers = new ArrayList<>();
+
+	        for (int i = 0; i < numThreads; i++) {
+	            DataHandler dataHandler = new DataHandler(observable);
+	            dataHandlers.add(dataHandler);
+	            Thread thread = new Thread(dataHandler);
+	            threads.add(thread);
+	            thread.start();
+	        }
+
+	        try {
+	            Thread.sleep(10000);
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+
+	        for (DataHandler handler : dataHandlers) {
+	            handler.stop();
+	        }
+
+	        for (Thread thread : threads) {
+	            try {
+	                thread.join();
+	            } catch (InterruptedException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
 
 }
